646fa40793e36893f98580fbc929494670754ea2
NagoyaL <nicolasgarcesl2004@hotmail.com>
2024-06-16 02:33:51 -0500

    Organizado

--- asoc_Deportes/utils/estadisticas2.py
+++ asoc_Deportes/utils/estadisticas2.py
@@ -1,30 +1,30 @@
 from utils.algortimos import quicksort_Algorit 
 from utils.algortimos import merge_sort
 
-def equipo_con_mayor_rendimiento(equipos):
+def equipo_con_mayor_rendimiento2(equipos):
     return max(equipos, key=lambda equipo: equipo.rendimiento_promedio())
 
-def equipo_con_menor_rendimiento(equipos):
+def equipo_con_menor_rendimiento2(equipos):
     return min(equipos, key=lambda equipo: equipo.rendimiento_promedio())
 
-def jugador_con_mayor_rendimiento(jugadores):
+def jugador_con_mayor_rendimiento2(jugadores):
     return max(jugadores, key=lambda jugador: jugador.rendimiento)
 
-def jugador_con_menor_rendimiento(jugadores):
+def jugador_con_menor_rendimiento2(jugadores):
     return min(jugadores, key=lambda jugador: jugador.rendimiento)
 
-def jugador_mas_joven(jugadores):
+def jugador_mas_joven2(jugadores):
     return min(jugadores, key=lambda jugador: jugador.edad)
 
-def jugador_mas_veterano(jugadores):
+def jugador_mas_veterano2(jugadores):
     return max(jugadores, key=lambda jugador: jugador.edad)
 
-def promedio_edad(jugadores):
+def promedio_edad2(jugadores):
     if jugadores:
         return sum(jugador.edad for jugador in jugadores) / len(jugadores)
     return 0
 
-def promedio_rendimiento(jugadores):
+def promedio_rendimiento2(jugadores):
     if jugadores:
         return sum(jugador.rendimiento for jugador in jugadores) / len(jugadores)
     return 0
@@ -32,13 +32,13 @@ def promedio_rendimiento(jugadores):
 
 
 
-def ordenar_equipos_en_sede(sede):
-    sede.equipos = merge_sort(sede.equipos, key=lambda equipo: (equipo.rendimiento_promedio(), -len(equipo.jugadores)))
+def ordenar_equipos_en_sede2(sede):
+    sede.equipos = quicksort_Algorit(sede.equipos, key=lambda equipo: (equipo.rendimiento_promedio(), -len(equipo.jugadores)))
 
-def ordenar_sedes(sedes):
-    sedes = merge_sort(sedes, key=lambda sede: (sede.rendimiento_promedio(), -sede.total_jugadores()))
+def ordenar_sedes2(sedes):
+    sedes = quicksort_Algorit(sedes, key=lambda sede: (sede.rendimiento_promedio(), -sede.total_jugadores()))
     return sedes
 
-def ordenar_jugadores_de_todas_las_sedes(sedes):
+def ordenar_jugadores_de_todas_las_sedes2(sedes):
     todos_jugadores = [jugador for sede in sedes for equipo in sede.equipos for jugador in equipo.jugadores]
-    return merge_sort(todos_jugadores, key=lambda jugador: (jugador.rendimiento, jugador.edad))
+    return quicksort_Algorit(todos_jugadores, key=lambda jugador: (jugador.rendimiento, jugador.edad))
